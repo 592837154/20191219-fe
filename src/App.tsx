09f75f738fc143424a642ccb563267ea2ab47ee3
zhukai <zhukai03@kuaishou.com>
2019-12-19 10:09:26 +0800

    20191219-fe

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import { message } from 'antd';
 import './App.css';
 
 const App: React.FC = () => {
+  message.success('朱凯fe');
+  message.success('create-react-app + typescript + antd');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          前端页面
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
