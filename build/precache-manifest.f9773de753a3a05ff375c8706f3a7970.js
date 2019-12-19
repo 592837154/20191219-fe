7a6539b3a2d2ecce9e07ebcc03604269b33fee40
zhukai <zhukai03@kuaishou.com>
2019-12-19 11:04:27 +0800

    a

---
+++ build/precache-manifest.f9773de753a3a05ff375c8706f3a7970.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9487d324255174998a61c4cd7d1f98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ffdab8d12cd383dcea",
+    "url": "/static/css/2.9ed5fc52.chunk.css"
+  },
+  {
+    "revision": "97779fd59f51aaf6edf6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b1ffdab8d12cd383dcea",
+    "url": "/static/js/2.1f171a12.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.1f171a12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97779fd59f51aaf6edf6",
+    "url": "/static/js/main.b31bbb67.chunk.js"
+  },
+  {
+    "revision": "d794b01935d824d5a1a6",
+    "url": "/static/js/runtime-main.d5bee788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
